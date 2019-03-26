ef1c0782eca27ee3081ec172598e69eeca1b144d
shridhar <shridhar.bennisur@costrategix.com>
2019-03-26 18:42:00 +0530

    delete api done

--- mongodbConfig/mongodb-find.js
+++ mongodbConfig/mongodb-find.js
@@ -5,9 +5,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
         return console.log('unable to connect', err);
     }
 
-    db.collection('users').find({
-        _id: new ObjectID('5c9a0e089c20714715bc756b')
-    }).toArray().then((users) => {
+    db.collection('users').find().toArray().then((users) => {
         console.log(JSON.stringify(users, undefined, 2));
     }).catch((err) => {
         console.log('unable to fetch users data', err);
