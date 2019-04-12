2b54d484c1cae9664942371db6198504415e0fb6
shridhar <shridhar.bennisur@costrategix.com>
2019-04-12 16:21:19 +0530

    configaration chnages

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -8,6 +8,6 @@ if (!process.env.MONGO_URL) {
 
 mongoose.Promise = global.Promise;
 // eslint-disable-next-line no-undef
-const url = mongodbConfig && mongodbConfig.url ? mongodbConfig.url : 'mongodb://localhost:27017/TodoApp';
+const url = mongodbConfig && mongodbConfig.url ? mongodbConfig.url : process.env.MONGO_URL;
 
 mongoose.connect(url, { useNewUrlParser: true });
